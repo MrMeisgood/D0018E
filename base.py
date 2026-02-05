670e95abdf22a39dde0f75bf63a87aa2500d14fa
MrMeisgood <albin.jankvist@gmail.com>
2026-02-05 18:38:30 +0100

    For use on aws

--- base.py
+++ base.py
@@ -3,19 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://temp.db"
-# db = SQLAlchemy(app)
-
-# Initial kladd databas config
-# class Todo(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.String(200), nullable=False)
-#     completed = db.Column(db.Integer, default=0)
-    # This is just here cause it might be good to have
-    # date_created = db.Column(db.DateTime, default=datetime.UTC)
-
-# def __repr__(self):
-#     return "<Task %r>" % self.id
 
 # Function that's ran when accessing index page
 @app.route("/")
@@ -24,7 +11,4 @@ def index():
 
 # Main
 if __name__ == "__main__":
-    # with app.app_context():
-    #     db.create_all()
-
-    app.run(debug=True, host="0.0.0.0", port=4000)
+    app.run(debug=True, host="0.0.0.0", port=4444)
