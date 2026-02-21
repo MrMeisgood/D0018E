e5bc96da57b1a7ee2beb59d2ea7c7f037c796d00
MrMeisgood <albin.jankvist@gmail.com>
2026-02-21 09:35:46 +0100

    Adding products to in_cart via buttons work now, we should work on displaying the cart at this point

--- base.py
+++ base.py
@@ -42,19 +42,22 @@ def index():
             conn = get_conn()
             cur = conn.cursor()
             # Get user_id
-            # NOTE: This is where things go wrong, check with Anton if the issue is obvious
-            cur.execute("SELECT user_id FROM users WHERE username = %s", (username))
-            user_id = cur.fetchone()
-            print(user_id)
-            # Check if product already exists
-            cur.execute("SELECT * FROM products WHERE product_id = %s AND users_id = %s", (product_id, user_id))
-            item = cur.fetchone()
-            if item:
+            cur.execute("SELECT users_id FROM users WHERE username = %s", (username))
+            user_id = cur.fetchone()[0]
+            # Check if product already exists in in_cart
+            print(product_id, user_id)
+            cur.execute("SELECT * FROM in_cart WHERE product_id = %s AND users_id = %s", (product_id, user_id))
+            test = cur.fetchone()
+            # Adds to in_cart or increases quantity
+            if test:
                 cur.execute("UPDATE in_cart SET quantity = quantity + 1 WHERE product_id = %s AND users_id = %s", (product_id, user_id))
             else:
-                cur.execute("INSERT INTO in_cart (users_id, product_id, quantity) VALUES (%s, %s, 1)", (user_id, product_id))
+                cur.execute("INSERT INTO in_cart (users_id, product_id, quantity) VALUES (%s, %s, 1);", (user_id, product_id))
+            conn.commit()
+            cur.close()
+            conn.close()
         except Exception:
-            return "ERROR HERE"
+            return "The database be strugglin'"
     # Doesn't get items till we actually know they're needed
     items = get_products()[:DISPLAYED_ITEMS]
     return render_template("index.html", name=username, items=items)
